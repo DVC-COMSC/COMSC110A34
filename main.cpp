691eafdce476ca5224aa41dd361a1f6b0072ef24
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 18:16:38 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  string month1, month2, month3;
+  double rainfall1, rainfall2, rainfall3;
+  double avg;
+  
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "The average rain fall for " << month1 << " " << month2 << " " << month3 << " is " << avg << endl;
+}
